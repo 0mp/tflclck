bb58c8e5a1b5d47daa0f0d981879069fbac446b8
pschz <mpp302@gmail.com>
2014-06-24 20:31:19 +0200

    Create tflclck.cpp

---
+++ tflclck.cpp
@@ -0,0 +1,133 @@
+/// tflclck
+/// version 1.15
+/// Mateusz Piotrowski
+/// 2014
+
+#include <time.h>
+#include <cstdio>
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+double version = 1.15;
+char input;
+clock_t start;
+double timeLeft;
+double duration;
+double previousTimeLeft;
+
+void instruction(){
+    printf("Decide what to do now:\n");
+    printf("Type 1 for Personal Preference Task.\n");
+    printf("Type 2 for Choice Task.\n");
+    printf("Type 3 for Read/Listen/Speak Campus Situation Task.\n");
+    printf("Type 4 for Read/Listen/Speak Academic Course Task.\n");
+    printf("Type 5 for Listen/Speak Campus Situation Task.\n");
+    printf("Type 6 for Listen/Speak Academic Course Task.\n");
+    printf("Type q to exit the program.\n");
+    printf("\n");
+}
+
+void timerCounter(double start, double amountOfTime){
+    while(timeLeft >= 0){
+        duration = (clock() - start) /(double) CLOCKS_PER_SEC;
+        timeLeft = amountOfTime -  duration;
+        timeLeft = (int)timeLeft;
+        if(previousTimeLeft != timeLeft && timeLeft >= 0)
+        {
+            previousTimeLeft = timeLeft;
+            printf("%.f\n", timeLeft);
+        }
+    }
+}
+
+void timer(int taskNumber, double preparation, double response){
+
+    system("cls");
+
+    printf("Task number %d\n\n", taskNumber);
+
+    start = clock();
+    previousTimeLeft = -1;
+    timeLeft = 1;
+
+    printf("PREPARATION\n");
+    printf("\a");
+    timerCounter(start, preparation);
+    printf("\a\a");
+    printf("\n");
+
+    start = clock();
+    previousTimeLeft = -1;
+    timeLeft = 1;
+
+    printf("Now get ready to give your response.\n");
+    timerCounter(start, 2);
+    printf("\n");
+
+    start = clock();
+    previousTimeLeft = -1;
+    timeLeft = 1;
+
+    printf("RESPONSE\n");
+    printf("\a");
+    timerCounter(start,response);
+    printf("\a\a");
+}
+
+
+int main(){
+    printf("Welcome to tflclck %.2f.\nThe TOEFL exam clock app.\nPlease, have a sit.\n\n", version);
+     do{
+        instruction();
+
+        input = getch();
+
+        switch(input){
+            case '1':
+                timer(1,15,45);
+                break;
+            case '2':
+                timer(2,15,45);
+                break;
+            case '3':
+                timer(3,30,60);
+                break;
+            case '4':
+                timer(4,30,60);
+                break;
+            case '5':
+                timer(5,20,60);
+                break;
+            case '6':
+                timer(6,20,60);
+                break;
+            case 't':{//test
+                double p, r;
+                cin >> p >> r;
+                timer(0,p,r);
+                break;
+                }
+            case 'q':
+                break;
+            default:{
+                system("cls");
+                printf("Please, try again.\n\n");
+                continue;
+                break;
+            }
+        }
+        printf("\n");
+        system("cls");
+        if(input != 'q')
+            printf("Time is up!\n\n");
+     }while(input != 'q');
+
+    printf("Bye bye Viet Nam.\n\n");
+    system("PAUSE");
+
+    return 0;
+}
